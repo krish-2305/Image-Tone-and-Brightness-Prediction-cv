f468815ff1eec3e7b895d5f96a337005ffb65fb2
Krishnan R <krishnanr2305@gmial.com>
2024-12-31 23:06:37 +0530

    File repo changing

--- code.py
+++ code.py
@@ -37,7 +37,7 @@ st.title("Image Tone and Brightness Prediction")
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
     
     # Predictions
     cool_warm_prediction, warm_percentage, cool_percentage = predict_cool_vs_warm(image)
